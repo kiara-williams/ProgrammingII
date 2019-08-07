f75b3760897382e7d913ed4a639f4aeb2d44e1e9
Kiara Williams <47551456+Hey-Red00@users.noreply.github.com>
2019-08-07 21:33:36 +1000

    Create broken_score.py
    Solution

---
+++ prac_01/broken_score.py
@@ -0,0 +1,16 @@
+"""
+CP1404/CP5632 - Practical
+Broken program to determine score status
+"""
+
+# TODO: Fix this!
+
+score = float(input("Enter score: "))
+while score < 0 or score > 100:
+  score = float(input("Enter a valid score: ""))
+if score >= 90:
+    print("Excellent")
+elif score >= 50:
+    print("Passable")
+else:
+    print("Bad")
