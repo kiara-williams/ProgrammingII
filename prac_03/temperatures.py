5e85168ffdb14d78cfea224cb47a7e0d4226905d
Kiara Williams <kiara.williams@outlook.com.au>
2019-08-22 09:40:01 +1000

    Refactor with functions

--- prac_03/temperatures.py
+++ prac_03/temperatures.py
@@ -8,18 +8,30 @@ def main():
     choice = input(">>> ").upper()
     while choice != "Q":
         if choice == "C":
-            celsius = float(input("Celsius: "))
-            fahrenheit = celsius * 9.0 / 5 + 32
-            print("Result: {:.2f} F".format(fahrenheit))
+            temperature = float(input("Celsius: "))
+            new_temperature = calculate_fahrenheit(temperature)
+            print("Result: {:.2f} F".format(new_temperature))
         elif choice == "F":
-            fahrenheit = float(input("Fahrenheit: "))
-            celsius = 5 / 9 * (fahrenheit - 32)
-            print("Result: {:.2f} C".format(celsius))
+            temperature = float(input("Fahrenheit: "))
+            new_temperature = calculate_celsius(temperature)
+            print("Result: {:.2f} C".format(new_temperature))
         else:
             print("Invalid option")
         print(MENU)
         choice = input(">>> ").upper()
     print("Thank you.")
+
+
+def calculate_celsius(temperature):
+    """Convert fahrenheit to celsius"""
+    new_temperature = 5 / 9 * (temperature - 32)
+    return new_temperature
+
+
+def calculate_fahrenheit(temperature):
+    """Convert celsius to fahrenheit"""
+    new_temperature = temperature * 9.0 / 5 + 32
+    return new_temperature
 
 
 main()
