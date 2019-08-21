3e27be4f6276fcf77085a205197e1941162d717f
Kiara Williams <kiara.williams@outlook.com.au>
2019-08-21 23:38:50 +1000

    Commit file to prac 03

---
+++ prac_03/broken_score.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 - Practical
+Program to determine score status
+"""
+
+def main():
+    score = float(input("Enter score: "))
+    while score < 0 or score > 100:
+        score = float(input("Enter a valid score: ""))
+    if score >= 90:
+        print("Excellent")
+    elif score >= 50:
+        print("Passable")
+    else:
+        print("Bad")
+
+main()
