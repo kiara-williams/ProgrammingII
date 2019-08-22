5b76ed430e968c6ea1d2b2a82899cba33be99d95
Kiara Williams <kiara.williams@outlook.com.au>
2019-08-22 12:29:43 +1000

    Add wildcard feature

--- prac_03/word_generator.py
+++ prac_03/word_generator.py
@@ -8,13 +8,42 @@ import random
 
 VOWELS = "aeiou"
 CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+MENU = """Enter "S" for standard substitution
+Enter "W" for wildcard substitution
+Enter "Q" to quit"""
 
-word_format = str(input("Enter a string of characters using a-z")).lower()
-word = ""
-for kind in word_format:
-    if kind not in VOWELS:
-        word += random.choice(CONSONANTS)
+print("This program will take a string of letters and use substitution to create a new string")
+print(MENU)
+selection = str(input("Choose an option")).upper()
+while selection != "Q":
+    word = ""
+    if selection == "S":
+        word_format = str(input("Enter a string of characters to substitute using a-z. \n>>>")).lower()
+        for kind in word_format:
+            if kind not in VOWELS:
+                word += random.choice(CONSONANTS)
+            else:
+                word += random.choice(VOWELS)
+            print(word)
+            print(MENU)
+            selection = str(input("Choose an option")).upper()
+    elif selection == "W":
+        word_format = str(input("Enter a string of characters using a-z. \nUse # for wildcard vowels and % for wildcard consonants. "
+                                "\nUse other special characters for a random letter. \n>>>")).lower()
+        for kind in word_format:
+            if kind == "#":
+                word += random.choice(VOWELS)
+            elif kind == "%":
+                word += random.choice(CONSONANTS)
+            elif kind not in VOWELS and kind not in CONSONANTS:
+                word += random.choice(VOWELS+CONSONANTS)
+            else:
+                word += kind
+        print(word)
+        print(MENU)
+        selection = str(input("Choose an option")).upper()
     else:
-        word += random.choice(VOWELS)
-
-print(word)
+        print("Invalid Option. Please select from the menu")
+        print(MENU)
+        selection = str(input("Choose an option")).upper()
+print("Goodbye")
