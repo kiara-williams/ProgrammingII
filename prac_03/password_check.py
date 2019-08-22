96d84b5c9cf6169b0c5e4872c7b351183bd949e0
Kiara Williams <kiara.williams@outlook.com.au>
2019-08-22 10:12:28 +1000

    Add function comments

--- prac_03/password_check.py
+++ prac_03/password_check.py
@@ -7,11 +7,13 @@ def main():
 
 
 def print_password(password):
+    """Iterate through range of password length to print stars"""
     for i in range(len(password)):
         print("*", end="")
 
 
 def get_password():
+    """Ask user for password input and checks password length against length constant"""
     print("Your password needs to contain at least {} characters".format(PASSWORD_LENGTH))
     password = str(input("Enter a password: "))
     while len(password) < PASSWORD_LENGTH:
