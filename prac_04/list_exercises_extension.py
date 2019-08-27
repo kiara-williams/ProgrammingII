83000b679097810fe2e85b8ea2a245810c64a67e
Kiara Williams <kiara.williams@outlook.com.au>
2019-08-27 10:20:47 +1000

    Add loop with escape condition for infinite number entry

---
+++ prac_04/list_exercises_extension.py
@@ -0,0 +1,28 @@
+"""Program that takes in x numbers and prints some interesting facts"""
+
+def main():
+    print("Enter as many numbers as you like. Enter a negative number when finished")
+    number_list = []
+    number_count = 0
+    new_number = int(input("Number 1: "))
+    if new_number >= 0:
+        number_list.append(new_number)
+        number_count += 1
+    while new_number >= 0:
+        new_number = int(input("Number {}: ".format(number_count + 1)))
+        if new_number >= 0:
+            number_list.append(new_number)
+            number_count += 1
+
+    if not number_list:
+        print("No numbers were entered")
+    else:
+        list_average = sum(number_list) / number_count
+        print("The first number is {}".format(number_list[0]))
+        print("The last number is {}".format(number_list[-1]))
+        print("The smallest number is {}".format(min(number_list)))
+        print("The largest number is {}".format(max(number_list)))
+        print("The average of these numbers is {:.1f}".format(list_average))
+
+
+main()
