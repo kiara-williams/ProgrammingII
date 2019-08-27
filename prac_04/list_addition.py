b5da05edb8e652753005c79dcacec1927ea44d0d
Kiara Williams <kiara.williams@outlook.com.au>
2019-08-27 13:06:03 +1000

    Refactor and add comments

--- prac_04/list_addition.py
+++ prac_04/list_addition.py
@@ -1,28 +1,38 @@
+"""Program that takes lists and adds the values together. Accommodates lists of different lengths"""
+
 def main():
-    list_one = [1, 2, 3]
+    list_one = [1, 2, 3, 4]
     list_two = [4, 5, 6]
     summed_values = []
     if len(list_one) == len(list_two):
+        """Adds lists of the same length"""
         for value in list_one:
-            add_value = list_two[list_one.index(value)]
-            new_value = value + add_value
+            new_value = add_values(list_one, list_two, value)
             summed_values.append(new_value)
     elif len(list_one) > len(list_two):
+        """Adds lists if list one is longer"""
         for value in list_one:
             try:
-                add_value = list_two[list_one.index(value)]
-                new_value = value + add_value
+                new_value = add_values(list_one, list_two, value)
                 summed_values.append(new_value)
             except IndexError:
+                """Catches last values"""
                 summed_values.append(value)
     else:
+        """Adds lists if list two is longer"""
         for value in list_one:
-            add_value = list_two[list_one.index(value)]
-            new_value = value + add_value
+            new_value = add_values(list_one, list_two, value)
             summed_values.append(new_value)
+        """Catches last values"""
         missed_values = list_two[value:]
         summed_values += missed_values
     print(list_one,"+", list_two,"=", summed_values)
+
+
+def add_values(list_one, list_two, value):
+    add_value = list_two[list_one.index(value)]
+    new_value = value + add_value
+    return new_value
 
 
 main()
