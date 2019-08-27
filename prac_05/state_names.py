6d69fa2238cc7e7b224d56b59c9cc40d6cd7cf30
Kiara Williams <kiara.williams@outlook.com.au>
2019-08-27 21:35:49 +1000

    Add table printing, debug

--- prac_05/state_names.py
+++ prac_05/state_names.py
@@ -4,15 +4,17 @@ State names in a dictionary
 File needs reformatting
 """
 
-# TODO: Reformat this file so the dictionary code follows PEP 8 convention
 STATE_NAMES = {"QLD": "Queensland", "NSW": "New South Wales", "NT": "Northern Territory", "WA": "Western Australia",
                "ACT": "Australian Capital Territory", "VIC": "Victoria", "TAS": "Tasmania"}
 # print(STATE_NAMES)
 
-state = input("Enter short state: ").upper()
+state = input("Enter short state or \"P\" to print all states: ").upper()
 while state != "":
-    if state in STATE_NAMES:
+    if state == "P":
+        for state in STATE_NAMES:
+            print("{:<3} is {:<}".format(state,STATE_NAMES[state]))
+    elif state in STATE_NAMES:
         print(state, "is", STATE_NAMES[state])
     else:
         print("Invalid short state")
-    state = input("Enter short state: ").upper()
+    state = input("Enter short state or \"P\" to print all states: ").upper()
