c26ccb0697ba1909ec78c8ab9c3ca64d3f6bed26
Kiara Williams <kiara.williams@outlook.com.au>
2019-08-28 22:18:30 +1000

    Cleanup formatting

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,17 +1,21 @@
 def main():
     words = {}
-    word_string = input("Enter a string")
+    longest_word = 0
+    word_string = input("Enter a string").lower()
     word_list = word_string.split()
     for word in word_list:
+        if len(word) > longest_word:
+            longest_word = len(word)
         if word in words:
             old_value = words[word]
             new_value = old_value + 1
             words[word] = new_value
         else:
             words[word] = 1
+    print("Text: {}".format(word_string))
     while len(words) > 0:
         current_word = min(words)
-        print(current_word, words[current_word])
+        print("{:<{}} = {}".format(current_word, longest_word, words[current_word]))
         words.pop(current_word)
 
 
