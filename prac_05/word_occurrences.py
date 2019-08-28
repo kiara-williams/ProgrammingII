c02f201a5a341a3cd4577f3ff7a97401f087557c
Kiara Williams <kiara.williams@outlook.com.au>
2019-08-28 22:32:36 +1000

    Add comments and cleanup

--- prac_05/word_occurrences.py
+++ prac_05/word_occurrences.py
@@ -1,3 +1,5 @@
+"""Program that takes a string and counts unique words"""
+
 def main():
     words = {}
     longest_word = 0
@@ -7,15 +9,19 @@ def main():
         if len(word) > longest_word:
             longest_word = len(word)
         if word in words:
+            """check if word in dict and modify value"""
             old_value = words[word]
             new_value = old_value + 1
             words[word] = new_value
         else:
             words[word] = 1
     print("Text: {}".format(word_string))
+    print("{:<{}}     {}".format("Word", longest_word, "Count"))
+    print("_________________________________")
     while len(words) > 0:
+        """Sort dict while printing"""
         current_word = min(words)
-        print("{:<{}} = {}".format(current_word, longest_word, words[current_word]))
+        print("{:<{}}  :  {}".format(current_word, longest_word, words[current_word]))
         words.pop(current_word)
 
 
