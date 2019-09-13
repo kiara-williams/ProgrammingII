822a161445dd2427cdbb4905c6cffcaaf9099a92
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-13 11:29:20 +1000

    Fix print issue

--- prac_06/languages.py
+++ prac_06/languages.py
@@ -6,3 +6,9 @@ def main():
     ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
     python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
     visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+
+    print(ruby)
+    print(python)
+    print(visual_basic)
+
+main()
