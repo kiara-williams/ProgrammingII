7e8bf6dd21a670346f30ae3494731bfcdac637e4
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-13 11:00:14 +1000

    Initial Commit

---
+++ prac_06/programming_language.py
@@ -0,0 +1,14 @@
+"""CP1404 Practical - Programming Language exercise"""
+
+class ProgrammingLanguage:
+    """Represents a programming language object"""
+
+    def __init__(self, name, typing, reflection, year):
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def is_dynamic(self):
+        return self.reflection
+
