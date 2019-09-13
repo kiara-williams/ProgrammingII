8bee25c2c4d8da4560d9f5490b65b0ab467cd405
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-13 11:06:43 +1000

    Initial Commit

---
+++ prac_06/languages.py
@@ -0,0 +1,8 @@
+"""CP1404 Practical - Programming Language exercise"""
+
+from prac_06.programming_language import ProgrammingLanguage
+
+def main():
+    ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+    python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+    visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
