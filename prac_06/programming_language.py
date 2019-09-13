234bd19e35b92f25b1f70cbc66228ffbb491dc81
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-13 12:10:52 +1000

    Cleanup

--- prac_06/programming_language.py
+++ prac_06/programming_language.py
@@ -3,17 +3,19 @@
 class ProgrammingLanguage:
     """Represents a programming language object"""
 
-    def __init__(self, name, typing, reflection, year):
+    def __init__(self, name="", typing="", reflection, year=0):
         self.name = name
         self.typing = typing
         self.reflection = reflection
         self.year = year
 
     def is_dynamic(self):
+        """Returns True if language typing is dynamic, false if not"""
         if self.typing == "Dynamic":
             return True
         else:
             return False
 
     def __repr__(self):
+        """String printing"""
         return "{}, {} Typing, Reflection={}, First appeared in {}".format(self.name, self.typing, self.reflection, self.year)
