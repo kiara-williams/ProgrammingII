f1e2c60dcc8d822458561b27b690c7dacbe7ed6b
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-13 17:05:43 +1000

    Refactor input request

--- prac_06/guitars.py
+++ prac_06/guitars.py
@@ -5,16 +5,10 @@ from prac_06.guitar import Guitar
 def main():
     guitars = []
     print("My guitars!")
-    #name = input("Name: ")
-    #while name != "":
-    #   name = input("Name: ")
-    #    year = int(input("Year: "))
-    #    cost = float(input("Cost: "))
-    #    print("{} ({}) : ${:.2f} added".format(name, year, cost))
-    #    guitars.append(Guitar(name, year, cost))
+    get_guitars(guitars)
 
-    guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
-    guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
+    #guitars.append(Guitar("Gibson L-5 CES", 1922, 16035.40))
+    #guitars.append(Guitar("Line 6 JTV-59", 2010, 1512.9))
 
     print("These are my guitars")
     for i, guitar in enumerate(guitars):
@@ -24,5 +18,15 @@ def main():
             vintage_string = ""
         print("Guitar {}: {:>20} ({}), worth ${:10,.2f}{}".format(i + 1, guitar.name, guitar.year, guitar.cost,
                                                                   vintage_string))
+
+def get_guitars(guitar_list):
+    name = input("Name: ")
+    while name != "":
+        year = int(input("Year: "))
+        cost = float(input("Cost: "))
+        print("{} ({}) : ${:.2f} added".format(name, year, cost))
+        guitar_list.append(Guitar(name, year, cost))
+        name = input("Name: ")
+    return guitar_list
 
 main()
