2b02dfa6b0acccc6e5af56845abe619310d05541
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-19 20:43:40 +1000

    Add sorting, commenting, cleanup

--- exercises/WEKAreader.py
+++ exercises/WEKAreader.py
@@ -1,8 +1,9 @@
-LOOPSTOP = "<=>"
+"""WEKA Reader for J48 Pre-formatted tree. I hate it too."""
+
+import operator
 
 def main():
     attributes = {}
-    attribute_list = []
     in_file = open('WEKAmodel.txt', 'r')
     for line in in_file:
         line.strip()
@@ -23,6 +24,7 @@ def main():
             attributes[current_attribute] += 1
         else:
             attributes[current_attribute] = 1
+    attributes = dict(sorted(attributes.items(), key=operator.itemgetter(1), reverse=True))
     for key in attributes:
         print('{} = {}'.format(key, attributes[key]))
     in_file.close()
