a0beb71a362c65c3d936012afb724ca352f70a89
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-20 22:55:11 +1000

    Add up and down button functionality

--- prac_07/miles_converter.py
+++ prac_07/miles_converter.py
@@ -12,5 +12,13 @@ class MileConverterApp(App):
         result = value * 1.6
         self.root.ids.output_label.text = "{:.2f}".format(result)
 
+    def add_one_value(self, value):
+        value += 1
+        self.root.ids.input_miles.text = "{:.2f}".format(value)
+
+    def subtract_one_value(self, value):
+        value -= 1
+        self.root.ids.input_miles.text = "{:.2f}".format(value)
+
 
 MileConverterApp().run()
