3c5fc1752aa9b46b11648904f2fbba1fbbb2ff0e
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-20 23:02:50 +1000

    Refactor

--- prac_07/miles_converter.py
+++ prac_07/miles_converter.py
@@ -1,9 +1,11 @@
 from kivy.app import App
 from kivy.lang import Builder
+from kivy.core.window import Window
 
 
 class MileConverterApp(App):
     def build(self):
+        Window.size = (400, 400)
         self.title = "Mile to Kilometre Converter"
         self.root = Builder.load_file('miles_converter.kv')
         return self.root
@@ -12,13 +14,9 @@ class MileConverterApp(App):
         result = value * 1.6
         self.root.ids.output_label.text = "{:.2f}".format(result)
 
-    def add_one_value(self, value):
-        value += 1
-        self.root.ids.input_miles.text = "{:.2f}".format(value)
-
-    def subtract_one_value(self, value):
-        value -= 1
-        self.root.ids.input_miles.text = "{:.2f}".format(value)
+    def increment_miles(self, increment):
+        increment += float(self.root.ids.input_miles.text)
+        self.root.ids.input_miles.text = "{:.2f}".format(increment)
 
 
 MileConverterApp().run()
