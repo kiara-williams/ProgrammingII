325f398fc3b9f7cda94ffe23314ee7f37a177c73
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-20 22:42:36 +1000

    Initial Commit

---
+++ prac_07/miles_converter.py
@@ -0,0 +1,16 @@
+from kivy.app import App
+from kivy.lang import Builder
+
+
+class MileConverterApp(App):
+    def build(self):
+        self.title = "Mile to Kilometre Converter"
+        self.root = Builder.load_file('miles_converter.kv')
+        return self.root
+
+    def convert_miles(self, value):
+        result = value * 1.6
+        self.root.ids.output_label.text = str(result)
+
+
+MileConverterApp().run()
