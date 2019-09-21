fa308e4ec963b40254088a93630e802c15120279
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-21 11:15:54 +1000

    Handle empty and invalid inputs, remove conversion button, add constant for conversion function

--- prac_07/miles_converter.py
+++ prac_07/miles_converter.py
@@ -2,20 +2,28 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.core.window import Window
 
+KMS = 1.6
+
 
 class MileConverterApp(App):
     def build(self):
-        Window.size = (400, 400)
+        Window.size = (400, 200)
         self.title = "Mile to Kilometre Converter"
         self.root = Builder.load_file('miles_converter.kv')
         return self.root
 
     def convert_miles(self, value):
-        result = value * 1.6
+        try:
+            result = float(value) * KMS
+        except ValueError:
+            result = 0
         self.root.ids.output_label.text = "{:.2f}".format(result)
 
     def increment_miles(self, increment):
-        increment += float(self.root.ids.input_miles.text)
+        if self.root.ids.input_miles.text == '':
+            increment += 0
+        else:
+            increment += float(self.root.ids.input_miles.text)
         self.root.ids.input_miles.text = "{:.2f}".format(increment)
 
 
