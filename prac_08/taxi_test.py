a446686425e1b59df4993613628c5ea246c3210d
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-27 22:02:31 +1000

    Initial Commit

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,10 @@
+from prac_08.taxi import Taxi
+
+my_car = Taxi("Prius 1", 100, 1.23)
+my_car.drive(40)
+print(my_car)
+print("Current fare is ${:.2f}".format(my_car.get_fare()))
+my_car.start_fare()
+my_car.drive(100)
+print(my_car)
+print("Current fare is ${:.2f}".format(my_car.get_fare()))
