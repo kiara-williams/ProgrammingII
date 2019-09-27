d2237c32e46d77e351b174d93046d74d7cca8e79
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-27 23:11:51 +1000

    Fix argument order issue

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -5,11 +5,11 @@ class SilverServiceTaxi(Taxi):
     flagfall = 4.5
 
     def __init__(self, name, fuel, fanciness):
-        super().__init__(fuel, name)
+        super().__init__(name, fuel)
         self.price_per_km = Taxi.price_per_km*fanciness
 
     def get_fare(self):
         return (self.price_per_km * self.current_fare_distance) + self.flagfall
 
     def __str__(self):
-        return "{} plus flagfall of {}".format(super().__str__(), self.flagfall)
+        return "{} plus flagfall of ${:.2f}".format(super().__str__(), self.flagfall)
