6c6693e358dca117bcb6debe68a48c9dc0d30985
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-27 23:14:12 +1000

    Adjust get fare function to use super()

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -9,7 +9,7 @@ class SilverServiceTaxi(Taxi):
         self.price_per_km = Taxi.price_per_km*fanciness
 
     def get_fare(self):
-        return (self.price_per_km * self.current_fare_distance) + self.flagfall
+        return super().get_fare() + self.flagfall
 
     def __str__(self):
         return "{} plus flagfall of ${:.2f}".format(super().__str__(), self.flagfall)
