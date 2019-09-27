3eac9b0f5bea22287079ca5f1461e72e10b3b209
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-27 22:32:05 +1000

    Initial Commit - testing file

---
+++ prac_08/unreliable_car_test.py
@@ -0,0 +1,10 @@
+from prac_08.unreliable_car import UnreliableCar
+
+my_car = UnreliableCar("'98 Civic", 120, 63)
+print(my_car)
+my_car.drive(30)
+print(my_car)
+my_car.drive(20)
+print(my_car)
+my_car.drive(50)
+print(my_car)
