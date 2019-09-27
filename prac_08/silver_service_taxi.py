d4201f9716e83acf9408b955dbb4bf93d48b2405
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-27 23:04:55 +1000

    Initial Commit

---
+++ prac_08/silver_service_taxi.py
@@ -0,0 +1,15 @@
+from prac_08.taxi import Taxi
+
+
+class SilverServiceTaxi(Taxi):
+    flagfall = 4.5
+
+    def __init__(self, name, fuel, fanciness):
+        super().__init__(fuel, name)
+        self.price_per_km = Taxi.price_per_km*fanciness
+
+    def get_fare(self):
+        return (self.price_per_km * self.current_fare_distance) + self.flagfall
+
+    def __str__(self):
+        return "{} plus flagfall of {}".format(super().__str__(), self.flagfall)
