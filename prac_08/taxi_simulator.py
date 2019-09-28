55e198de7e69d01a89cc01c195282259f569a8a4
Kiara Williams <kiara.williams@outlook.com.au>
2019-09-28 18:02:50 +1000

    Add choose taxi functionality

--- prac_08/taxi_simulator.py
+++ prac_08/taxi_simulator.py
@@ -1,17 +1,33 @@
 from prac_08.silver_service_taxi import SilverServiceTaxi
+from prac_08.taxi import Taxi
 
 MENU = "q)uit, c)hoose taxi, d)rive"
 
 def main():
     taxis = [Taxi("Prius", 100), SilverServiceTaxi("Limo", 100, 2), SilverServiceTaxi("Hummer", 200, 4)]
+    bill = 0
+    current_taxi = None
     print("Let's drive!")
     menu_selection = get_menu_choice()
     while menu_selection != 'q':
         if menu_selection == 'c':
+            for taxi in taxis:
+                print(taxi)
+            while current_taxi is None:
+                try:
+                    taxi_selection = int(input("Choose taxi:"))
+                except ValueError:
+                    print("You need to enter a number")
+                    taxi_selection = int(input("Choose taxi:"))
+                except IndexError:
+                    print("That taxi is not in the list")
+                    taxi_selection = int(input("Choose taxi:"))
+                current_taxi = taxis[taxi_selection]
+        else:
             # finish
-        if menu_selection == 'd':
-            #finish
-        get_menu_choice()
+
+        print(bill)
+        menu_selection = get_menu_choice()
 
 
 def get_menu_choice():
