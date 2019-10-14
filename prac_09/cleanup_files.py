9fdc404acf45e1bf56ef3f9e93b8e9790c892614
Kiara Williams <kiara.williams@outlook.com.au>
2019-10-14 22:41:11 +1000

    Initial Cleaned Commit

--- prac_09/cleanup_files.py
+++ prac_09/cleanup_files.py
@@ -1,42 +1,9 @@
 """
 CP1404/CP5632 Practical
-Demos of various os module examples
+File Cleanup Program
 """
-import shutil
+
 import os
-
-
-def main():
-    """Demo os module functions."""
-    print("Starting directory is: {}".format(os.getcwd()))
-
-    # Change to desired directory
-    os.chdir('Lyrics/Christmas')
-
-    # Print a list of all files in current directory
-    print("Files in {}:\n{}\n".format(os.getcwd(), os.listdir('.')))
-
-    # Make a new directory
-    # The next time you run this, it will crash if the directory exists
-    try:
-        os.mkdir('temp')
-    except WindowsError:
-        pass
-
-    # Loop through each file in the (current) directory
-    for filename in os.listdir('.'):
-        # Ignore directories, just process files
-        if os.path.isdir(filename):
-            continue
-
-        new_name = get_fixed_filename(filename)
-        print("Renaming {} to {}".format(filename, new_name))
-
-        # Option 1: rename file to new name - in place
-        # os.rename(filename, new_name)
-
-        # Option 2: move file to new place, with new name
-        # shutil.move(filename, 'temp/' + new_name)
 
 
 def get_fixed_filename(filename):
@@ -45,7 +12,7 @@ def get_fixed_filename(filename):
     return new_name
 
 
-def demo_walk():
+def main():
     """Process all subdirectories using os.walk()."""
     os.chdir('Lyrics')
     for directory_name, subdirectories, filenames in os.walk('.'):
@@ -59,5 +26,4 @@ def demo_walk():
             os.rename(file_name, new_name)
 
 
-# main()
-demo_walk()
+main()
